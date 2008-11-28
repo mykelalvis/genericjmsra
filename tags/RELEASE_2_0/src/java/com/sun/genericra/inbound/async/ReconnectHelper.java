7c31f4bf9f547f7b7b4195f3222145c7273a9d6e
nobody <nobody@1f35156f-e848-60a1-e4d9-e6a90167dbe3>
2008-11-28 11:40:52 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_2_0'.

---
+++ tags/RELEASE_2_0/src/java/com/sun/genericra/inbound/async/ReconnectHelper.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2004-2005 Sun Microsystems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.sun.genericra.inbound.async;
+
+import com.sun.genericra.inbound.*;
+import com.sun.genericra.util.*;
+
+import java.util.logging.*;
+
+import javax.jms.*;
+
+import javax.resource.*;
+import javax.resource.spi.*;
+
+
+/**
+ * Helper class that enables redelivery in case connection
+ * generates an Exception.
+ *
+ * @author Binod P.G
+ */
+public class ReconnectHelper implements ExceptionListener {
+    private static Logger _logger;
+
+    static {
+        _logger = LogUtils.getLogger();
+    }
+
+    private InboundJmsResourcePool pool = null;
+    private EndpointConsumer consumer = null;
+    private int attempts = 0;
+    private long interval = 0;
+    private boolean reconnectInitiated = false;
+
+    public ReconnectHelper(InboundJmsResourcePool pool,
+        EndpointConsumer consumer) {
+        this.pool = pool;
+        this.consumer = consumer;
+        this.attempts = consumer.getSpec().getReconnectAttempts();
+        this.interval = consumer.getSpec().getReconnectInterval() * 1000;
+    }
+
+    public void onException(JMSException exception) {
+        if (reconnectInitiated || this.consumer.isStopped()) {
+            _logger.log(Level.INFO, "Reconnect is in progress");
+
+            return;
+        } else {
+            reconnectInitiated = true;
+        }
+
+        _logger.log(Level.INFO, "Reconnecting now");
+        _logger.log(Level.INFO, "Reconnecting now");
+        _logger.log(Level.INFO, "Reconnecting now");
+         if (reestablishPool()) {
+                // this.consumer.closeConsumer();
+                // this.consumer.restart();
+                _logger.log(Level.INFO, "Reconnected!!");
+
+                // TO DO i18n
+            } else {
+                _logger.log(Level.SEVERE, "Reconnect failed in pool!!");
+
+                // TO DO i18n 
+            }
+    }
+    private void createConsumer() throws ResourceException {
+        try {
+                this.consumer.closeConsumer();
+                this.consumer.restart();
+        } catch (ResourceException re) {
+                _logger.log(Level.INFO,"Reconnection failed, Cannot create consumer");
+                try {
+                        this.pool.stop();
+                        // this.consumer.stop();
+                }catch (Exception e) {
+                        _logger.log(Level.SEVERE, "Reconnect failed while stopping pool");
+                }
+                _logger.log(Level.INFO,"Stopping the consumer");
+                throw re;
+        }
+    }
+
+    /**
+     * Pause all waiting threads. recreate the serversession pool.
+     */
+    private boolean reestablishPool() {
+        try {
+            this.pool.stop();
+        } catch (JMSException je) {
+            _logger.log(Level.SEVERE, "Reconnect failed while stopping pool");
+            je.printStackTrace();
+
+            // TO DO log a message 
+            return false;
+        } catch (Exception e) {
+            _logger.log(Level.SEVERE,
+                "Reconnect failed while stopping pool " + e);
+
+            // TO DO log a message 
+        }
+
+        boolean result = false;
+
+        for (int i = 0; i < attempts; i++) {
+            _logger.log(Level.INFO, "Reconnect attempt->" + i);
+
+            try {
+                this.pool.initialize();
+                this.pool.releaseAllWaitingThreads();
+                createConsumer();
+                _logger.log(Level.INFO, "Reconnect successful with pool->" + i);
+                result = true;
+
+                break;
+            } catch (ResourceException re) {
+                _logger.log(Level.INFO,
+                    "Reconnect attempt failed. Now sleeping for" + interval);
+
+                // TODO. log a message.
+                try {
+                    Thread.sleep(interval);
+                } catch (Exception e) {
+                    _logger.info("Thread.sleep exception");
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public InboundJmsResourcePool getPool() {
+        return this.pool;
+    }
+}
---
+++ tags/RELEASE_2_0/src/java/com/sun/genericra/monitoring/ResourceMonitor.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright 2004-2005 Sun Microsystems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+
+package com.sun.genericra.monitoring;
+
+import java.util.Hashtable;
+
+
+import com.sun.genericra.inbound.AbstractJmsResourcePool;
+
+import com.sun.genericra.inbound.async.InboundJmsResourcePool;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import com.sun.genericra.util.LogUtils;
+
+
+/**
+ * MBEan implemenation to monitor the inbound resource pool.
+ * 
+ */
+public class ResourceMonitor implements ResourceMonitorMBean {
+    
+    public Hashtable poolstatistics;
+    
+    public static Logger logger;
+    
+    
+    static {
+        logger = LogUtils.getLogger();
+    }
+    /** Creates a new instance of ResourceMonitor */
+    public ResourceMonitor() {
+        poolstatistics = new Hashtable();
+    }
+    
+    public void addPool(String consumer, AbstractJmsResourcePool ps) {
+        
+        if ((ps != null) && (consumer != null)) {
+            try {
+                poolstatistics.put(consumer, ps);
+            } catch (Throwable t ){
+                
+                t.printStackTrace();
+                logger.log(Level.SEVERE, "Exception while initializing " +
+                        "monitoring for endpoint " + consumer);
+            }
+            logger.log(Level.FINE, "Including endpoint " + consumer + " for monitoring");
+        }
+    }
+    
+    public void removePool(String consumer) {
+        try {
+            poolstatistics.remove(consumer);
+        } catch (Throwable t) {
+            logger.log(Level.SEVERE, "Unable to remove " + consumer +
+                    " from monitoring table");
+        }
+    }
+    
+    private boolean validate(String appname) {
+        if ((appname == null) || (appname.trim().equals(""))) {
+            return false;
+        }
+        return true;
+    }
+    
+    public String getPoolStatistics(String appname) {
+        if (!validate(appname)) {
+            return "Invalid application name";
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return "Endpoint not found OR could not be monitored";
+        }
+        PoolStatistics ps = new PoolStatistics((InboundJmsResourcePool)pool);
+        return ps.formatStatistics();
+    }
+    
+    private AbstractJmsResourcePool getPool(String name) {
+        AbstractJmsResourcePool pool = null;
+        try {
+            pool = (AbstractJmsResourcePool) poolstatistics.get(name.trim());
+        } catch (Exception e) {
+            ;
+        }
+        return pool;
+    }
+    /**
+     * Getter for property currentSize.
+     * @return Value of property currentSize.
+     */
+    public  int getCurrentSize(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getCurrentResources();
+    }
+    
+    /**
+     * Getter for property busyResources.
+     * @return Value of property busyResources.
+     */
+    public int getBusyResources(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getBusyResources();
+    }
+    
+    
+    /**
+     * Getter for property freeResources.
+     * @return Value of property freeResources.
+     */
+    public int getFreeResources(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getFreeResources();
+    }
+    
+    /**
+     * Getter for property waiting.
+     * @return Value of property waiting.
+     */
+    public int getWaiting(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getWaiting();
+    }
+    
+    public int getConnections(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getConnectionsInUse();
+    }
+    
+    public int getMaxSize(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return pool.getMaxSize();
+    }
+    
+    public long getMaxWaitTime(String appname) {
+        if (!validate(appname)) {
+            return -1;
+        }
+        AbstractJmsResourcePool pool = getPool(appname);
+        if (pool == null) {
+            return -2;
+        }
+        return  pool.getMaxWaitTime();
+    }
+    
+}
