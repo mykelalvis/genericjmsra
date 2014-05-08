b439006e40431d2131ebd1c2f5bddef0b6fbfae4
simeng_oracle <simeng_oracle@1f35156f-e848-60a1-e4d9-e6a90167dbe3>
2014-05-08 02:40:28 +0000

    RELEASE_2_0_1_a

---
+++ tags/RELEASE_2_0_1_a/src/java/com/sun/genericra/XAResourceType.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2004-2005 Sun Microsystems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.sun.genericra;
+
+import com.sun.genericra.inbound.async.EndpointConsumer;
+import com.sun.genericra.util.*;
+
+import java.io.Serializable;
+
+import java.lang.reflect.Method;
+
+import java.security.*;
+
+import java.util.*;
+import java.util.logging.*;
+
+import javax.jms.*;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ActivationSpec;
+import javax.resource.spi.BootstrapContext;
+import javax.resource.spi.ResourceAdapter;
+import javax.resource.spi.ResourceAdapterInternalException;
+import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.resource.spi.work.WorkManager;
+
+import javax.transaction.xa.XAResource;
+
+
+/**
+ * Interface used by all XAResource objecrts that are wrapped.
+ *
+ * @author Binod P.G
+ */
+public interface XAResourceType {
+    /**
+     * Retrieve the XAResource object wrapped.
+     */
+    public Object getWrappedObject();
+
+    /**
+     * Set the Resource Manager policy
+     */
+    public void setRMPolicy(String policy);
+
+    /**
+     * Retrieve the RM policy
+     */
+    public String getRMPolicy();
+
+    /**
+     * If any one of the resources are configured with
+     * a policy of "OneForPhysicalConnection", then
+     * compare physical connection. Otherwise, return true
+     * so that the actual XAResource wrapper can delegate it
+     * to the underlying XAResource implementation.
+     */
+    public boolean compare(XAResourceType type);
+
+    /**
+     * Retrieves the physical JMS connection object.
+     */
+    public Connection getConnection();
+
+    /**
+     * Set the physical jms connection object associated with
+     * this XAResource wrapper
+     */
+    public void setConnection(Connection con);
+}
---
+++ tags/RELEASE_2_0_1_a/src/java/com/sun/genericra/inbound/AbstractJmsResource.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2004-2005 Sun Microsystems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.sun.genericra.inbound;
+import com.sun.genericra.GenericJMSRA;
+import com.sun.genericra.inbound.*;
+import com.sun.genericra.inbound.async.DeliveryHelper;
+import com.sun.genericra.util.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.jms.*;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.*;
+import javax.resource.spi.endpoint.*;
+import javax.resource.spi.work.*;
+
+import javax.transaction.xa.XAResource;
+/**
+ *
+ * @author rp138409
+ */
+public abstract class AbstractJmsResource {
+    protected static Logger _logger;
+    
+    static {
+        _logger = LogUtils.getLogger();
+    }
+    
+    protected Session session;
+    protected XAResource xaresource;
+    protected MessageEndpoint endPoint;
+    protected boolean free;
+    protected GenericJMSRA ra;
+    protected AbstractJmsResourcePool pool;
+    protected DeliveryHelper helper;
+    
+    public AbstractJmsResource(Session session, AbstractJmsResourcePool pool,
+            XAResource xaresource) throws JMSException {
+        this.session = session;
+        this.xaresource = xaresource;
+        this.pool = pool;
+        this.ra = (GenericJMSRA) pool.getConsumer().getResourceAdapter();
+    }
+    public AbstractJmsResourcePool getPool() {
+        return this.pool;
+    }
+    
+    public XASession getXASession() {
+        return (XASession) session;
+    }
+      public XAResource getXAResource() {
+        return this.xaresource;
+    }  
+    public MessageEndpoint getEndpoint() {
+        return this.endPoint;
+    }
+}
